134e47569932319ba184c27a86ee7583d0e05ce8
Yi Changsheng <ycs.knight@outlook.com>
2020-12-16 17:06:50 +0800

    feature: jdbc 工具

--- src/main/java/com/cug/util/JDBCUtil.java
+++ src/main/java/com/cug/util/JDBCUtil.java
@@ -1,4 +1,30 @@
 package com.cug.util;
 
+import java.sql.*;
+
 public class JDBCUtil {
+    private Connection connection;
+
+    public JDBCUtil(String driverUrl) throws ClassNotFoundException {
+        Class.forName(driverUrl);
+    }
+
+    public Connection getConnection(String url, String userName, String userPassword) throws SQLException {
+        if(this.connection == null){
+            this.connection = DriverManager.getConnection(url, userName, userPassword);
+        }
+        return this.connection;
+    }
+
+    public void close(Connection connection, Statement statement, ResultSet resultSet) throws SQLException {
+        if(connection != null && ! connection.isClosed()){
+            connection.close();
+        }
+        if(statement != null){
+            statement.close();
+        }
+        if(resultSet != null){
+            resultSet.close();
+        }
+    }
 }
